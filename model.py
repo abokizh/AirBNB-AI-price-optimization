81faa40cfb0a67bc4e89ff554b47d99462bed1f7
Abdulaziz Bokijonov <aziz@Abdulazizs-MacBook-Air.local>
2024-10-17 20:29:49 -0400

    Working model (MVP)

---
+++ model.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBRegressor
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+import lightgbm as lgb
+from catboost import CatBoostRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('accommodation_data.csv')
+
+# Drop NAs
+df_cleaned = df.dropna()
+
+# Add occupancy percentage column and drop booked and available columns
+df_cleaned["occupancy"] = 100 * df_cleaned['booked']/(df_cleaned['available']+df_cleaned['booked'])
+
+
+Q1_price = df_cleaned['price'].quantile(0.25)
+Q3_price = df_cleaned['price'].quantile(0.75)
+IQR_price = Q3_price - Q1_price
+
+Q1_occupancy = df_cleaned['occupancy'].quantile(0.25)
+Q3_occupancy = df_cleaned['occupancy'].quantile(0.75)
+IQR_occupancy = Q3_occupancy - Q1_occupancy
+
+
+# Define the bounds for outliers
+lower_bound_price = Q1_price - 1.5 * IQR_price
+upper_bound_price = Q1_price + 1.5 * IQR_price
+
+
+lower_bound_occupancy = Q1_occupancy - 1.5 * IQR_occupancy
+upper_bound_occupancy = Q3_occupancy + 1.5 * IQR_occupancy
+
+# Filter out rows that have 'price' and 'occupancy' outside these bounds
+df_no_outliers = df_cleaned[
+    (df_cleaned['price'] >= lower_bound_price) & (df_cleaned['price'] <= upper_bound_price) &
+    (df_cleaned['occupancy'] >= lower_bound_occupancy) & (df_cleaned['occupancy'] <= upper_bound_occupancy)
+    ]
+
+print(lower_bound_occupancy, upper_bound_occupancy)
+
+# Features (X) and target (y)
+X = df_no_outliers.drop(columns=['price', 'booked', 'available', 'id'])  # Assuming 'price' is the target variable
+y = df_no_outliers['price']
+
+print(len(X))
+
+# If there are categorical columns, use LabelEncoder to convert them to numeric (as a simple approach)
+label_encoders = {}
+for column in X.select_dtypes(include=['object']).columns:
+    label_encoders[column] = LabelEncoder()
+    X[column] = label_encoders[column].fit_transform(X[column])
+
+# Split the dataset into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(X_train)
+
+### Initialize the XGBRegressor model ###
+xgb_model = XGBRegressor(max_depth=3, learning_rate=0.1, n_estimators=100)
+
+# Hyperparameter tuning 
+param_distributions = {
+    'max_depth': [3, 4, 5, 6],
+    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+    'n_estimators': [100, 200, 300],
+    'subsample': [0.7, 0.8, 0.9, 1.0],
+    'colsample_bytree': [0.7, 0.8, 0.9, 1.0],
+    'min_child_weight': [1, 2, 3, 4]
+}
+xgb_model = RandomizedSearchCV(xgb_model, param_distributions, scoring='neg_mean_squared_error', n_iter=100, cv=3, random_state=42)
+
+
+# Train the model
+xgb_model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_test_pred = xgb_model.predict(X_test)
+
+# Make predictions on the training set
+y_train_pred = xgb_model.predict(X_train)
+
+# Evaluate the model (using Mean Squared Error here as a metric)
+test_mse = mean_squared_error(y_test, y_test_pred)
+train_mse = mean_squared_error(y_train, y_train_pred)
+
+print(f'XGBRegression Test RMSE: {test_mse**(1/2)}')
+print(f'XGBRegression Train RMSE: {train_mse**(1/2)}\n')
+
+# # Plot feature importance
+# xgb.plot_importance(xgb_model.best_estimator_)  # 'best_estimator_' if using RandomizedSearchCV
+# plt.show()
+
+user_input = {
+    'guests': [2],
+    'rooms': 2,
+    'beds': 1,
+    'baths': 1.5,
+    'occupancy': 70
+}
+
+user_input_df = pd.DataFrame(user_input)
+print(xgb_model.predict(user_input_df)[0])
---
+++ web_parser.py
@@ -0,0 +1,105 @@
+from lxml import html
+import requests
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import random
+import re
+from selenium.common.exceptions import NoSuchElementException
+
+class Parser:
+
+    links = list()
+
+    def __init__(self, links):
+        self.links = links
+
+    
+    def get_js_data(self, link):
+        # Set up the webdriver (you need a driver like ChromeDriver or GeckoDriver)
+        driver = webdriver.Chrome()  # or webdriver.Firefox(), etc.
+
+        id = link.split("/")[-1]
+
+        # Open the Airbnb page
+        driver.get(link)
+
+        # Wait for the dynamic content to load (can use WebDriverWait for better control)
+        driver.implicitly_wait(2)
+
+        # Check if an error page is displayed or data is missing
+        error_elements = driver.find_elements(By.XPATH, '//div[contains(text(), "Oops")]')
+        if error_elements:
+            raise IndexError("505 Error: Page not available")
+        
+
+        # Now you can use XPath to get dynamic elements
+        booked = len(driver.find_elements(By.XPATH, '//div[@data-is-day-blocked="true"]'))
+        available = len(driver.find_elements(By.XPATH, '//div[@data-is-day-blocked="false"]'))
+
+        features_raw = driver.find_elements(By.XPATH, '//section/div/ol[contains(@class, "dir-ltr")]/li[contains(@class, "dir-ltr")]')
+        features = dict()
+        for f in features_raw:
+            string = f.text.split(" ")
+            string = [s for s in string if s not in ['·', '']]
+
+            if len(string) == 1:
+                if string[0] == "Studio":
+                    features["bedrooms"] == 0
+            else:
+                (value, feature) = string
+                features[feature] = value
+
+        price_raw = driver.find_elements(By.XPATH, '//div[contains(@style, "--pricing-guest-display-price-alignment")]/div/span/div/span[@class and normalize-space(@class) != ""]')
+        price = float((price_raw[2].text).replace("$", ""))
+
+        data = {
+            "id": id,
+            "booked": booked,
+            "available": available,
+            "features": features,
+            "price": price
+        }
+        # for element in elements:
+        #     print(element.text)
+
+        # Close the browser
+        driver.quit()
+
+        return data
+
+    def parse(self, link):
+        js_data = self.get_js_data(link)
+
+        return js_data
+
+    def run(self):
+        data = list()
+        
+        for l in self.links:
+            try:
+                data.append(self.parse(l))
+            except Exception as e:
+                print(f"Error parsing {l}: {e}")  # Print the error message and continue
+        return data
+
+# # Save JSON to a file
+# with open("data3.json", 'w') as json_file:
+#     json.dump(json_data, json_file, indent=4)
+
+# # Loop over the list and write each JSON string to a separate file
+# for i, json_str in enumerate(availability):
+#     # Check if the string is not empty
+#     if json_str.strip():  # Only process non-empty strings
+#         try:
+#             # Convert JSON string to Python dictionary
+#             json_obj = json.loads(json_str)
+#             # Create a filename dynamically based on the index
+#             filename = f'output_file_{i}.json'
+#             # Open the file in write mode
+#             with open(filename, 'w') as file:
+#                 # Write the dictionary to the file as a JSON string
+#                 json.dump(json_obj, file, indent=4)
+#             print(f"Successfully wrote {filename}")
+#         except json.JSONDecodeError as e:
+#             print(f"Skipping invalid JSON string: {json_str} - Error: {e}")
